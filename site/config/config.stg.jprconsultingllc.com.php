da50ea20bca54b2c1206d5df8c819a15b33b3d65
Paul Wagner <paulw@foodchain.com>
2023-07-18 17:07:41 -0700

    jprconsulting stg config

---
+++ site/config/config.stg.jprconsultingllc.com.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'debug' => true,
+    'url' => 'https://stg.jprconsultingllc.com',
+    'cache' => [
+        'pages' => [
+            'active' => true,
+        ]
+    ],
+];
