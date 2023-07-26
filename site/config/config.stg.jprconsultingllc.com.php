7a1968b11a07a3a29f5239533c3e6287319f1c49
Paul Wagner <paulw@foodchain.com>
2023-07-26 12:35:51 -0700

    created plugin to copy content from stg -> prod configured it to interact with webhooks plugin

--- site/config/config.stg.jprconsultingllc.com.php
+++ site/config/config.stg.jprconsultingllc.com.php
@@ -8,4 +8,13 @@ return [
             'active' => true,
         ]
     ],
+    //deployment webhook config
+    'pju.webhook-field.endpoint' => env('WEBHOOKS_ENDPOINT'),
+    'pju.webhook-field.hooks' => [
+        env('DEPLOY_CONTENT_HOOK') => [
+          'url' => '/'.env('DEPLOY_CONTENT_HOOK').'/start',
+          'payload' => ['key' => env('DEPLOY_CONTENT_KEY')],
+          'showOutdated' => false
+        ]
+    ],
 ];
