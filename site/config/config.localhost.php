ee3bcca68bd70fcc8da59d6b6f944f5567d21905
Paul Wagner <paulw@foodchain.com>
2023-07-26 12:34:30 -0700

    remove flailing attempt to fix what was really an issue with the core config of nginx https://getkirby.com/docs/cookbook/setup/nginx should be: 	try_files $uri $uri/ /index.php$is_args$args;
    not:
    	try_files $uri $uri/ /index.php?$uri&$args;
    
    issue #3976 had nothing to do with the issue

--- site/config/config.localhost.php
+++ site/config/config.localhost.php
@@ -11,19 +11,3 @@ return [
         ]
     ],
 ];
-
-
-// Kirby::plugin('my/plugin', [
-//   'siteMethods' => [
-//     /* returns true if logged-in user and their role is in $roles (can be string or array) */
-//     'userIs' => function ($roles) {
-//       // the cookie check can be removed once core issue #3976 is resolved
-//       if (!empty(Cookie::get(option('session.cookieName')))) {
-//         if (kirby()->user() && in_array(kirby()->user()->role()->id(), (array)$roles)) {
-//           return true;
-//         }
-//       }
-//       return false;
-//     },
-//   ],
-// ]);
